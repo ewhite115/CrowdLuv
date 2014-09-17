6ae2ba724f456286463d6346690412a4988ddcb7
ewhite115 <ewhite115@gmail.com>
2014-09-17 19:11:19 -0400

    switch email input on infoform to type email

--- inc/userinfoform.php
+++ inc/userinfoform.php
@@ -7,7 +7,7 @@
                 <div class="row">
                     <div class="col-ms-5">
                         <p>Email Address</p>
-                        <input class="cl_graybackground" type="text" name="email" id="email" value="<?php echo $CL_LOGGEDIN_USER_OBJ['email'];?>"></input>
+                        <input class="cl_graybackground" type="email" name="email" id="email" value="<?php echo $CL_LOGGEDIN_USER_OBJ['email'];?>"></input>
                     </div>
                     <div class="col-ms-7">
                         <p>Allow CrowdLuv Contact?</p>
@@ -20,7 +20,7 @@
                     <div class="col-ms-5">
                         <p>Mobile Number</p>
                         <input class="cl_graybackground" type="tel" name="mobile" id="mobile" value="<?php echo $CL_LOGGEDIN_USER_OBJ['mobile'];?>"></input>
-                        <p2 class="cl-textcolor-standout">Receive SMS alerts when your favorite performers come to your town</p2>
+                        <p2 class="cl-textcolor-standout">Please provide a valid mobile number to receive SMS alerts when your favorite performers come to your town</p2>
                         <span id="phone_valid"></span>
                     </div>
                     <div class="col-ms-7">
@@ -33,7 +33,7 @@
 
                 <div class="row">
                     <div class="col-xs-12">
-                        <br><label for="firstname">Name</label><input size="10" class="cl_graybackground" type="text" name="firstname" id="firstname" value="<?php echo $CL_LOGGEDIN_USER_OBJ['firstname'];?>"><input size="10" class="cl_graybackground" type="text" name="lastname" id="lastname" value="<?php echo $CL_LOGGEDIN_USER_OBJ['lastname'];?>">
+                        <br><label for="firstname">Name</label><input size="10" disabled class="cl_graybackground" type="text" name="firstname" id="firstname" value="<?php echo $CL_LOGGEDIN_USER_OBJ['firstname'];?>"><input size="10" class="cl_graybackground" type="text" disabled name="lastname" id="lastname" value="<?php echo $CL_LOGGEDIN_USER_OBJ['lastname'];?>">
                         <br><label for="city">Your City</label><input class="cl_graybackground" type="text" name="city" id="city" value="<?php echo $CL_LOGGEDIN_USER_OBJ['location_fbname'];?>" disabled></td>
                         <br><label for="birthdate">Birth date</label><input class="cl_graybackground" type="text" name="birthdate" id="birthdate" disabled value="<?php echo $CL_LOGGEDIN_USER_OBJ['birthdate'];  ?>">
                         <br><label for="rltsp">Relationship</label><input class="cl_graybackground" type="text" name="rltsp" id="rltsp" disabled value="<?php echo $CL_LOGGEDIN_USER_OBJ['fb_relationship_status'];  ?>">

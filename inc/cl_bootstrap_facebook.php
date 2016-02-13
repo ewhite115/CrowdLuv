ca22979fac40a141e3e6353edd72b64085889941
ewhite115 <ewhite115@gmail.com>
2016-02-13 03:47:01 -0500

    refactor - remove unnecessary include

--- inc/cl_bootstrap_facebook.php
+++ inc/cl_bootstrap_facebook.php
@@ -1,6 +1,6 @@
 <?php
 
-  require_once(ROOT_PATH . 'vendor/autoload.php');
+  //require_once(ROOT_PATH . 'vendor/autoload.php');
   use Facebook\FacebookSession;
   use Facebook\FacebookRequest;
   use Facebook\FacebookRedirectLoginHelper;

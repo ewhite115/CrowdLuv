128524c0c822a81a63ce6bc8f2e81346a845be6c
ewhite115 <ewhite115@gmail.com>
2016-02-18 20:45:16 -0500

    routing for index, pages, brands

--- public/front.php
+++ public/front.php
@@ -4,6 +4,56 @@ require_once "../inc/cl_bootstrap.php";
 error_reporting(E_ALL);
 
 
+//echo "D";
+
+$klein = new \Klein\Klein();
+
+
+//Landing page
+$klein->respond('/', function () use($clRequestInformation, $clResponseInformation, $CL_model, $clFacebookHelper) {
+    
+    require "../pages/index.php";
+    return;
+
+});
+
+
+$klein->respond('/brand/[:brandName]', function ($kleinRequest) use($clRequestInformation, $clResponseInformation, $CL_model, $clFacebookHelper) {
+    
+    require "../pages/brand.php";
+    return;
+
+});
+
+$klein->respond('/brand/[:brand]/cometomytown', function ($kleinRequest) use($clRequestInformation, $clResponseInformation, $CL_model, $clFacebookHelper) {
+    
+    require "../pages/brand_cometomytown.php";
+    return;
+
+});
+
+
+//Default fallback to look for a mathcing page script in the /pgaes directory
+$klein->respond('/[:pagename]', function ($request) use($clRequestInformation, $clResponseInformation, $CL_model, $clFacebookHelper) {
+    
+    require "../pages/" . $request->pagename;
+    return;
+
+});
+
+
+
+$klein->dispatch();
+return;
+
+
+
+
+
+
+
+
+
 
 //Determine route
 $clPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
